3b8160594dc24739e5cfdfd3bd5b0d51f4b4d162
Al <jalo1412@gmail.com>
2021-01-20 19:39:54 +0100

    feat(index): add listed date

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const puppeteer = require('puppeteer');
 const print = require('./printHelper')
 
+async function firstListingDateExtractor(page) {
+     let liArray = await page.evaluate(() => {
+        return Array.from(
+            document.querySelectorAll('#detailBullets_feature_div > ul > li'),
+            element => {
+                if (element.querySelector('span > span:first-child').textContent.includes('Im Angebot von Amazon.de seit'))
+                    return element.querySelector('span > span:nth-child(2)').textContent
+            }
+        )
+    })
+    let trArray = await page.evaluate(() => {
+        return Array.from(
+            document.querySelectorAll('#productDetails_detailBullets_sections1 > tbody > tr'),
+            element => {
+                if (element.querySelector('th').textContent.includes('Im Angebot von Amazon.de seit'))
+                    return element.querySelector('td').textContent.replace(/\n/g, '')
+            }
+        )
+    })
+    return liArray.find(li => li !== null) ? liArray.find(li => li !== null) : trArray.find(li => li !== null)
+}
+
 async function getPDPProductInfo(browser, pageLink) {
     try {
         const page = await browser.newPage();
@@ -10,17 +32,21 @@ async function getPDPProductInfo(browser, pageLink) {
             const title = document.querySelector('#productTitle') ? document.querySelector('#productTitle').textContent.replace(/\n/g, '') : null;
             const price = document.querySelector('#priceblock_ourprice') ? document.querySelector('#priceblock_ourprice').textContent : null;
             const avgRating = document.querySelector('#acrPopover') ? document.querySelector('#acrPopover').title : null
-            const numberOfReviews = document.querySelector('#acrCustomerReviewText') ? document.querySelector('#acrCustomerReviewText').textContent : null
+            const numberOfReviews = document.querySelector('#acrCustomerReviewText') ? document.querySelector('#acrCustomerReviewText').textContent : null            //const firstListingDate = document.querySelector('#detailBullets_feature_div') ? document.querySelector('#detailBullets_feature_div > ul > li:nth-child(3) > span > span:nth-child(2)').textContent : null
+
             return ({
-                //@TODO ADD FIRST LISTING DATE AMAZON
                 title: title,
                 price: price,
                 avgRating: avgRating,
                 numberOfReviews: numberOfReviews,
             })
         });
+
+        product.firstListingDate = await firstListingDateExtractor(page)
         product.link = pageLink
+
         return product
+
     } catch {
     }
 }
@@ -91,5 +117,6 @@ async function scrappeOnAmazon(product, numberOfSearchPages, numberOfSimultaneou
     const productPDPUrls = await getPDPUrlsInNSearchResultPages(numberOfSearchPages, product)
     return await getProducts(productPDPUrls, numberOfSimultaneousPDP)
 }
-//scrappeOnAmazon('nintendo 64', 2, 10).then((r)=>console.log(r))
+
+//scrappeOnAmazon('anker', 1, 10).then((r) => console.log(r))
 module.exports = scrappeOnAmazon;
